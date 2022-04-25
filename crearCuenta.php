78ab5c118b3917bb05c6bb016835e44f8484a1c0
jose <joseluisgm1998@gmail.com>
2022-04-25 17:13:39 -0500

    push

--- crearCuenta.php
+++ crearCuenta.php
@@ -10,11 +10,14 @@ $recibido=0;
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	    <link rel="stylesheet" type="text/css" href="css/estilo.css">
 	    <!-- Bootstrap CSS -->
+	
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/boo tstrap.bundle.min.js"></script> 
 	    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	    <!-- Data Table -->
 	    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
 	    <!-- Icons -->
 	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
 		<script defer src="js/crearCuenta.js"></script>
 	    <title>Crear Cuenta</title>
   	</head>
