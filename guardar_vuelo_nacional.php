5b6bd58a5845493308cea727df4a93a089f5430c
SebasLuna13 <sebastian.urbano1@utp.edu.co>
2022-05-12 13:26:20 -0500

    subir fotos en vuelo

--- guardar_vuelo_nacional.php
+++ guardar_vuelo_nacional.php
@@ -6,6 +6,11 @@
     $$var=$val;
     }
 
+	$nom_archivo = $_FILES["foto_vuelo"]["name"];
+	$ruta = "img/".$nom_archivo;
+	$archivo = $_FILES["foto_vuelo"]["tmp_name"];
+	$subir = move_uploaded_file($archivo, $ruta);
+
 	$consulta = "INSERT INTO vuelo(id_tipo_vuelo, fecha_hora_salida, id_nacional_origen, id_nacional_destino, costo_vuelo, foto_vuelo, id_estado, id_cant_horas) 
 		VALUES ('1', '$fecha_hora_salida', '$id_nacional_origen', '$id_nacional_destino', '$costo_vuelo', '$foto_vuelo', '1', '$id_cant_horas')";
 	mysqli_query($enlace, $consulta);
