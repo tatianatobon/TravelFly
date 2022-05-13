b86f60accec5d61074f9b457401a0b28357de711
SebasLuna13 <sebastian.urbano1@utp.edu.co>
2022-05-12 22:09:50 -0500

    subir foto_vuelo_nacional

--- guardar_vuelo_nacional.php
+++ guardar_vuelo_nacional.php
@@ -6,13 +6,13 @@
     $$var=$val;
     }
 
-	$nom_archivo = $_FILES["foto_vuelo"]["name"];
-	$ruta = "img/".$nom_archivo;
-	$archivo = $_FILES["foto_vuelo"]["tmp_name"];
+	$nom_imagen = $_FILES['foto_vuelo']['name'];
+	$ruta = "img/".$nom_imagen;
+	$archivo = $_FILES['foto_vuelo']['file'];
 	$subir = move_uploaded_file($archivo, $ruta);
 
 	$consulta = "INSERT INTO vuelo(id_tipo_vuelo, fecha_hora_salida, id_nacional_origen, id_nacional_destino, costo_vuelo, foto_vuelo, id_estado, id_cant_horas) 
-		VALUES ('1', '$fecha_hora_salida', '$id_nacional_origen', '$id_nacional_destino', '$costo_vuelo', '$foto_vuelo', '1', '$id_cant_horas')";
+		VALUES ('1', '$fecha_hora_salida', '$id_nacional_origen', '$id_nacional_destino', '$costo_vuelo', '$nom_imagen', '1', '$id_cant_horas')";
 	mysqli_query($enlace, $consulta);
 	mysqli_close($enlace);
 	header("Location: listar_vuelo_nacional.php");
