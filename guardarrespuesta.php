78a41a32be122491c5f9012d8d5f614910089444
Cristian Orlando <cristian.ramirez1@utp.edu.co>
2022-05-19 20:38:18 -0500

    Yagit add .!

---
+++ guardarrespuesta.php
@@ -0,0 +1,14 @@
+<?php
+	include('conexion.php');
+
+	foreach ($_REQUEST as $var => $val) {
+	$$var=$val;
+	}
+	
+		$consulta = "UPDATE foro SET repuestaAdministrador = '$Respuesta' WHERE foro . id_mensaje ='$id_mensaje'";
+
+		mysqli_query($enlace, $consulta);
+		header("Location: foro2.php");
+	
+	mysqli_close($enlace);
+?>	
