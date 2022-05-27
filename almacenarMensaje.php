19d4c9e121c9eebaf4c14a8d69eafc9e7a97831d
SebasLuna13 <sebastian.urbano1@utp.edu.co>
2022-05-26 19:35:05 -0500

    guaradar mensaje foro

--- almacenarMensaje.php
+++ almacenarMensaje.php
@@ -7,6 +7,7 @@
 	
 	$consulta = "INSERT INTO foro (id_mensaje, user, mensaje, respuestaAdministrador) VALUES (NULL, '$user', '$mensaje', NULL)";
 	mysqli_query($enlace, $consulta);
-	header("Location: forousuario.php");
 	mysqli_close($enlace);
+	header("Location: forousuario.php");
+
 ?>	
